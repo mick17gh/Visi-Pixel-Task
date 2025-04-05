e8281d7e2ae2cb8b611f87de395a8727723d6dfd
mick17gh <mike17gh@gmail.com>
2025-04-05 23:03:31 +0000

    1.updated endpoint url

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -29,7 +29,7 @@ export class ProductsController {
     }
   }
 
-  @Get('slug/:slug')
+  @Get('detail/:slug')
   getProductBySlug(@Param('slug') slug: string) {
     try {
       return this.productsService.getProductBySlug(slug);
